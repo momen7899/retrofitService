b21b6ea6bd9a98cde62bb322a23f222286e81838
momen.hamaveysi <momen.hamaveysi@kubakgroup.com>
2021-08-14 17:22:50 +0430

    Create MVVM

--- presentation/src/main/java/com/momen/retrofitservice/view/MainActivity.kt
+++ presentation/src/main/java/com/momen/retrofitservice/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.momen.retrofitservice
+package com.momen.retrofitservice.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.momen.retrofitservice.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
